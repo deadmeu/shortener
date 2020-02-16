ca74e981f51b7576f577c7881c5936c19076bc67
deadmeu <deadmeu@gmail.com>
2020-02-17 00:36:25 +1000

    Updates

---
+++ precache-manifest.1366bbc341d465b9f8d173584db28b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7388c84ff7c184966acff686cf0adcbd",
+    "url": "/shortener/index.html"
+  },
+  {
+    "revision": "6be7b990ee6712fd09c3",
+    "url": "/shortener/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "0ef0795401d5bd2a73f2",
+    "url": "/shortener/static/js/2.03c65912.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/shortener/static/js/2.03c65912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be7b990ee6712fd09c3",
+    "url": "/shortener/static/js/main.5c16c62f.chunk.js"
+  },
+  {
+    "revision": "fb3cbffebaf674b09832",
+    "url": "/shortener/static/js/runtime-main.b7ec5a13.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shortener/precache-manifest.bfbd40543e1ca3cef4fa4405642dabb9.js"
+  "/shortener/precache-manifest.1366bbc341d465b9f8d173584db28b51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shortener/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
